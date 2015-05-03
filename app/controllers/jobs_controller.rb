224823412ff35657b3f001feb57ea6a08588f33e
Bryan <Bpadron2000@gmail.com>
2015-05-03 09:18:20 -0400

    added stuff

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   def new
-    @job = Job.new(:id => 1)
+    @job = Job.new
   end
   def show
 
@@ -11,14 +11,7 @@ class JobsController < ApplicationController
   end
   def create
 
-    @job = Job.new(:job_data)
-    @job.save
-    render plain: params[:jobs].inspect
     redirect_to job_path
 
   end
-  private
-  def params
-    job_data = params.require(:job).permit(:name, :description, :picture_url)
   end
-end
