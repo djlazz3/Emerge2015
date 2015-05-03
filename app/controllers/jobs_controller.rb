28178145346c3496987609a1eb21c38920a8d04f
Bryan <Bpadron2000@gmail.com>
2015-05-03 08:35:03 -0400

    added facebook button

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,14 +1,24 @@
 class JobsController < ApplicationController
   def new
-    @job = Job.new
+    @job = Job.new(:id => 1)
   end
   def show
 
   end
+  def index
+    @job = Job.all
+
+  end
   def create
-    job_data = params.require(:job).permit(:name, :description, :picture_url)
-    @job = Job.new job_data
+
+    @job = Job.new(:job_data)
     @job.save
-    redirect_to jobs_show_path
+    render plain: params[:jobs].inspect
+    redirect_to job_path
+
+  end
+  private
+  def params
+    job_data = params.require(:job).permit(:name, :description, :picture_url)
   end
 end
